025754af6f78b7b32cf1ff413bcdc0801ea454a7
sushant <ksushant6566@gmail.com>
2021-05-27 21:33:34 +0530

    backend production

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+const app = express();
+
+// connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+// app.use(methodOverride('_method'));
+
+const PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+  res.json({ msg: "hello" });
+});
+
+// Define Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/users', require('./routes/users'))
+app.use('/api/ambulances',require('./routes/ambulance'))
+app.use('/api/bookings', require('./routes/bookings'))
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
